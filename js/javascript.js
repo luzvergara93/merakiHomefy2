fa2f20e86eea16cfbfa0da9f46cb22f7fcdbc98f
Luz Vergara <luzvergara_16@hotmail.com>
2021-10-23 22:06:20 -0300

    agregue inspiration

--- js/javascript.js
+++ js/javascript.js
@@ -66,3 +66,73 @@ $('#bedroom').click( () => {
     searchImg('bedroom decoration');
 });
 
+
+
+////////////////// Contacto /////////////////////
+
+$(document).ready(() => {
+   
+    $('#contacto').submit(function() {
+        let nombre = $('#nombre').val().trim();
+        let apellido = $('#apellido').val().trim();
+        
+        alert(`Hola ${nombre} ${apellido}, hemos recibido tu consulta, en breve nos comunicaremos con vos para asesorarte. \n Meraki Homefy`)
+     
+    });
+
+
+    $('#reset').click(function(){
+        $('input').val('');
+    });
+});
+
+
+//////////////////////// Inspiration //////////
+
+var apiKey = '23955266-33c9870af76f27031e4d59e56';
+
+let searchImg = (word) => { 
+    
+    var URL = "https://pixabay.com/api/?key="+apiKey+"&q="+encodeURIComponent(word);
+    
+    $.getJSON(URL, function(data){
+        if (parseInt(data.totalHits) > 0)
+            $.each(data.hits, function(i, hit){ 
+                console.log(hit.largeImageURL); 
+
+                var img = $('<img />', { 
+                    id: 'photo',
+                    src: `${hit.largeImageURL}`,
+                    alt: 'MyAlt'
+                });
+                img.appendTo($('#photos'));
+            });
+        else
+            console.log('No hits');
+        } );
+}
+
+$('#living').click( () => {
+    $('#photos').empty();
+    searchImg('living decoration');
+});
+
+$('#kitchen').click( () => {
+    $('#photos').empty();
+    searchImg('kitchen decoration');
+});
+
+$('#bathroom').click( () => {
+    $('#photos').empty();
+    searchImg('bathroom decoration');
+});
+
+$('#bedroom').click( () => {
+    $('#photos').empty();
+    searchImg('bedroom decoration');
+});
+
+
+
+
+
--- js/productos.js
+++ js/productos.js
@@ -12,15 +12,28 @@ class producto {
 
 //array productos para aromas
 const aromas = [
-new producto(1, 'imgs/aromas/aroma1.jpg', 'Difusor tapa madera', 'madera', 1200),
-new producto(2, 'imgs/aromas/aroma2.jpg' , 'Dispenser para jabon', 'clear', 800),
-new producto(3, 'imgs/aromas/aroma3.jpg', 'Perfumina Tilo', 'vino', 600),
-new producto(4, 'imgs/aromas/aroma4.jpg' ,'Vela aromatica', 'Blanco', 300),
-new producto(5, 'imgs/aromas/aroma5.jpg' ,'Repuesto Difusor', 'Surtido', 600),
-new producto(6, 'imgs/aromas/aroma6.jpg' ,'Difusor Vidrio', 'Clear', 500),
-new producto(7, 'imgs/aromas/aroma7.jpg' ,'Frasco con Vela', 'Blanco', 300),
+    new producto(1, 'imgs/aromas/aroma1.jpg', 'Difusor tapa madera', 'madera', 1200),
+    new producto(2, 'imgs/aromas/aroma2.jpg' , 'Dispenser para jabon', 'clear', 800),
+    new producto(3, 'imgs/aromas/aroma3.jpg', 'Perfumina Tilo', 'vino', 600),
+    new producto(4, 'imgs/aromas/aroma4.jpg' ,'Vela aromatica', 'Blanco', 300),
+    new producto(5, 'imgs/aromas/aroma5.jpg' ,'Repuesto Difusor', 'Surtido', 600),
+    new producto(6, 'imgs/aromas/aroma6.jpg' ,'Difusor Vidrio', 'Clear', 500),
+    new producto(7, 'imgs/aromas/aroma7.jpg' ,'Frasco con Vela', 'Blanco', 300),
 ]
 
+const deco = [
+    new producto(1, 'imgs/deco/deco1.jpg', 'Envase Cookies', 'Estampado', 600),
+    new producto(2, 'imgs/deco/deco2.jpg', 'Portacubiertos Marmol', 'Marmol', 550),
+    new producto(3, 'imgs/deco/deco3.jpg', 'Bandeja Marmol', 'Marmol', 880),
+    new producto(4, 'imgs/deco/deco4.jpg', 'Mate Simil Cuero', 'Beige', 600),
+    new producto(5, 'imgs/deco/deco5.jpg', 'Portarretratos Metal', 'Plata', 700),
+    new producto(6, 'imgs/deco/deco6.jpg', 'Portarretratos Madera', 'Madera', 500),
+    new producto(7, 'imgs/deco/deco7.jpg', 'Caja de Te ISABEL', 'Blanco', 900),
+    new producto(8, 'imgs/deco/deco8.jpg', 'Mantel Rosario', 'Manteca', 600),
+    new producto(9, 'imgs/deco/deco9.jpg', 'Fanal Luciernaga', 'Dorado', 500),
+    new producto(10, 'imgs/deco/deco10.jpg', 'Combo baño', 'Vison', 1100),
+
+]
 
 // creo las imagenes
 const contenedor = document.getElementById("contenedorAromas");
@@ -28,34 +41,32 @@ const contenedor = document.getElementById("contenedorAromas");
 
 const mostrarProductos = (array) => {
 
-array.forEach((producto) => {
-    const div = document.createElement('div');
-    div.className = "card"
-    div.style = "width: 15rem"
+            array.forEach((producto) => {
+                const div = document.createElement('div');
+                div.className = "card"
+                div.style = "width: 15rem"
 
-    div.innerHTML = `
-                    <img src=${producto.img} class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title"> ${producto.nombre} </h5>
-                        <p class="card-text"> Precio: $ ${producto.precio} </p>
-                        
-                    </div>
-                    
-                    `
-    const comprar = document.createElement('button');
-    comprar.classList.add('btn', 'btn-primary');
-    comprar.textContent = 'Comprar';
-    comprar.setAttribute('marcador', producto.id);
-    comprar.addEventListener('click', addToCart);
-    div.appendChild(comprar);
-    contenedor.appendChild(div);
-
-
-});
-
+                div.innerHTML = `
+                                <img src=${producto.img} class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title"> ${producto.nombre} </h5>
+                                    <p class="card-text"> Precio: $ ${producto.precio} </p>
+                                    
+                                </div>
+                                
+                                `
+                const comprar = document.createElement('button');
+                comprar.classList.add('btn', 'btn-primary');
+                comprar.textContent = 'Comprar';
+                comprar.setAttribute('marcador', producto.id);
+                comprar.addEventListener('click', addToCart);
+                div.appendChild(comprar);
+                contenedor.appendChild(div);
+            });
 }
 
 mostrarProductos(aromas);
+
 
 let carrito = [];
 let total = 0;
